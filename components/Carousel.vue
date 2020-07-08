b5e1b3fbf7a37c5ac495eb9f4b841591c8afb7e7
ra-md <ra799m@gmail.com>
2020-07-08 15:43:20 +0700

    replace carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,32 +1,111 @@
 <template>
-	<client-only>
-		<vue-carousel class="my-4" :data="data" indicator-trigger="hover" />
-	</client-only>
+  <div class="relative">
+    <div class="carousel my-4">
+      <h1 v-if="trendings.length === 0">Loading...</h1>
+      <VueSlickCarousel v-else v-bind="settings">
+        <div v-for="trending in trendings" :key="trending.id">
+          <div class="trending px-2">
+            <div class="bg">
+              <img class="rounded-md" :src="`https://image.tmdb.org/t/p/w500/${trending.backdrop_path}`">
+            </div>
+            <h1 class="title">
+              {{ trending.title||trending.name }}
+            </h1>
+          </div>
+        </div>
+      </VueSlickCarousel>
+    </div>
+  </div>
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import api from '~/api';
+
 export default {
+  components: {
+    VueSlickCarousel
+  },
   data() {
     return {
-      data: [
-        '<div class="example-slide">Slide 1</div>',
-        '<div class="example-slide">Slide 2</div>',
-        '<div class="example-slide">Slide 3</div>'
-      ]
+      trendings: [],
+      settings: {
+        dots: true,
+        centerMode: true,
+        infinite: true,
+        autoplay: true,
+        slidesToShow: 2,
+        speed: 500,
+        autoplaySpeed: 5000,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              slidesToShow: 1
+            }
+          }
+        ]
+      }
     };
+  },
+  mounted() {
+    api.trending('all', 'week')
+    .then((res) => {
+      const data = res.data.results.splice(0, 5);
+
+      this.trendings = data;
+    });
   }
 };
 </script>
 
 <style>
-  .example-slide {
+  .slick-prev:before, .slick-next:before {
+    color: black;
+  }
+
+  .slick-slide div div {
+    outline: none;
+  }
+
+  .trending, .bg {
+    position: relative;
+    cursor: pointer;
+  }
+
+  .bg::before {
     @apply rounded-md;
-    align-items: center;
-    background-color: #666;
-    color: #999;
-    display: flex;
-    font-size: 1.5rem;
-    justify-content: center;
-    min-height: 15rem;
+    content: '';
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    transition: background-color 0.5s;
+  }
+
+  .trending:hover .bg::before {
+    background-color: rgba(0,0,0,0.6);
+  }
+
+  .trending .title {
+    display: none;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
+    text-align: center;
+  }
+
+  .trending:hover .title {
+    display: inline-block;
+  }
+
+  @media (max-width: 768px) {
+    .trending .title {
+      font-size: 16px;
+    }
   }
 </style>
