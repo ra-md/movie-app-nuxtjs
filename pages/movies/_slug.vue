1e75326e1d7028dcb65ae9d2b84f798db3cdc4d7
ra-md <ra799m@gmail.com>
2020-08-01 15:17:39 +0700

    use asyncData

--- pages/movies/_slug.vue
+++ pages/movies/_slug.vue
@@ -19,38 +19,47 @@ export default {
 	components: {
 		Detail
 	},
-	data() {
-		return {
-			movie: {},
-			cast: [],
-			trailer: {},
-			similarMovies: []
-		};
+	async asyncData(context) {
+		try {
+			const slug = context.params.slug.split('-');
+			const movieId = slug[slug.length - 1];
+
+			const [credits, details, trailer, similar] = await Promise.all([
+				api.credits('movie', movieId),
+				api.details('movie', movieId),
+				api.trailer('movie', movieId),
+				api.similar('movie', movieId)
+			]);
+
+			return {
+				cast: credits.data.cast,
+				movie: details.data,
+				trailer: trailer.data.results[0],
+				similarMovies: similar.data.results
+			};
+		} catch (error) {
+			context.error({ statusCode: 404, message: 'Page Not Found' });
+		}
 	},
-	created() {
-		const slug = this.$route.params.slug.split('-');
-		const movieId = slug[slug.length - 1];
+	// created() {
+	// 	const slug = this.$route.params.slug.split('-');
+	// 	const movieId = slug[slug.length - 1];
 
-		api.details('movie', movieId)
-		.then((res) => {
-			this.movie = res.data;
-		});
+	// 	api.details('movie', movieId)
+	// 	.then((res) => {
+	// 		this.movie = res.data;
+	// 	});
 
-		api.credits('movie', movieId)
-		.then((res) => {
-			this.cast = res.data.cast;
-		});
+	// 	api.trailer('movie', movieId)
+	// 	.then((res) => {
+	// 		this.trailer = res.data.results[0];
+	// 	});
 
-		api.trailer('movie', movieId)
-		.then((res) => {
-			this.trailer = res.data.results[0];
-		});
-
-		api.similar('movie', movieId)
-		.then((res) => {
-			this.similarMovies = res.data.results;
-		});
-	},
+	// 	api.similar('movie', movieId)
+	// 	.then((res) => {
+	// 		this.similarMovies = res.data.results;
+	// 	});
+	// },
 	head() {
 		return {
 			titleTemplate: this.movie.title
